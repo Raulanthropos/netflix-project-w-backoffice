0851f71259fb998945ebd72bdff63e1e9b2e3db8
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-13 22:22:45 +0200

    POST works

--- js/backoffice.js
+++ js/backoffice.js
@@ -1,37 +1,75 @@
-async function onFormSubmit(event) {  //Asynchronous function declaration, to be used inline in HTML
-    event.preventDefault(); // Default form behavior is to refresh the page; that way, the data you input and you try to submit by clicking the button "Submit", get lost. Hence,
-    // preventDefault() behavior - this is a built in js function.
+const moviesUrl = "https://striveschool-api.herokuapp.com/api/movies/";
+const horrorMoviesUrl = "https://striveschool-api.herokuapp.com/api/movies/horror";
+const comedyMoviesUrl = "https://striveschool-api.herokuapp.com/api/movies/comedy";
+const musicMoviesUrl = "https://striveschool-api.herokuapp.com/api/movies/music";
 
-    const newMovie = {  //This is an object to be used later on, with the keys being the ones found in the API objects
-      name: document.querySelector("#name").value,
-      description: document.querySelector("#description").value,
-      category: document.querySelector("#genre").value,
-      imageUrl: document.querySelector("#imageUrl").value,
+
+
+const getallHorrorMovies = async() => {
+  const response = await fetch(horrorMoviesUrl, {headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+    "Content-Type": "application/json"
+  }});
+  const listOfMovies = response.json();
+  console.log(listOfMovies);
+}
+
+const getallComedyMovies = async() => {
+  const response = await fetch(comedyMoviesUrl, {headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+    "Content-Type": "application/json"
+  }});
+  const listOfMovies = response.json();
+  console.log(listOfMovies);
+}
+
+const getallMusicMovies = async() => {
+  const response = await fetch(musicMoviesUrl, {headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+    "Content-Type": "application/json"
+  }});
+  const listOfMovies = response.json();
+  console.log(listOfMovies);
+}
+
+const onFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const newMovie = {
+      name: document.getElementById("e-name").value,
+      description: document.getElementById("description").value,
+      category: document.getElementById("category").value,
+      imageUrl: document.getElementById("image-url").value,
     };
 
-    const options = { // The declaration of the 2nd parameter, to be used in a future fetch
+    const options = {
       method: "POST",
-      body: JSON.stringify(newMovie), // This converts the newMovie into json, to easily be implemented to the API, when needed
+      body: JSON.stringify(newMovie),
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZkMTAyNGQ0YmUzZDAwMTU4NDYwZjgiLCJpYXQiOjE2NjgwOTE5NDEsImV4cCI6MTY2OTMwMTU0MX0.et2nZ7DRAkHpdooekdUVz7Z5CeAw4TyIwGYk1teIv-A",
-        "Content-Type": "application/json",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+        "Content-Type": "application/json"
       },
     };
 
-    try { //The try/catch is totally optional, it's for error handling.
-      const endpoint = "https://striveschool-api.herokuapp.com/api/movies/";
-      const response = await fetch(endpoint, options);
-      if (response.ok) { //This is to better the UX, since without an alert, we complete the actions and don't know for certain if the change was made, or not
-        alert("Your movie has been added!");
+    try {
+      const response = await fetch(moviesUrl, options);
+      if (response.ok) {
+        alert("Your movie was added successfully!");
+        console.log(moviesUrl);
       } else {
-        throw new Error("Error! Try again in one minute.");
+        throw new Error("Error! Please try again!");
       }
     } catch (error) {
       console.log(error);
     }
   }
+
   window.onload = async () => {
-    const appointments = await getallMovies(); // Where is the getAllMovies function?
-    renderAppointments(appointments); // Where is the renderAppointments function?
+    await getallHorrorMovies();
+    await getallMusicMovies();
+    await getallComedyMovies();
   };
--- js/script.js
+++ js/script.js
@@ -4,70 +4,180 @@ const musicBtn = document.getElementById("music-btn");
 let selectedGenre = document.getElementById("selected-genre");
 const movieCoverGenre = document.querySelectorAll(".movie-cover-genre");
 const hiddenPrompts = document.querySelectorAll(".d-none");
+const horrorUrl = "https://striveschool-api.herokuapp.com/api/movies/horror";
+const comedyUrl = "https://striveschool-api.herokuapp.com/api/movies/comedy";
+const musicUrl = "https://striveschool-api.herokuapp.com/api/movies/music";
+const modalWindow = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
 
 const options = {
-    method: "GET",
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
-    },
-  };
-
-const getMovieGenres = async () =>
-{
-    const fetchGenres = await fetch("https://striveschool-api.herokuapp.com/api/movies", options);
-    const genresData = await fetchGenres.json();
-    console.log(genresData, genresData[0], genresData[1], genresData[2]);
-}
+  method: "GET",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+  },
+};
 
 const getHorrorMovies = async () => {
-    const fetchTheHorror = await fetch("https://striveschool-api.herokuapp.com/api/movies/horror", options);
-    const horrorData = await fetchTheHorror.json();
-    const categoryData = horrorData.map(data => data.category);
-    const imgData = horrorData.map(data => data.imageUrl);
-    console.log(imgData);
-    selectedGenre.innerText = categoryData[1];
-    movieCoverGenre.forEach(img => {
-        img.innerHTML = `<img class="movie-cover movie-cover-genre" src="${imgData}">`
-    })
-    hiddenPrompts.forEach(prompt => {
-        prompt.classList.remove("d-none");
-    })
-}
+  const fetchTheHorror = await fetch(
+    horrorUrl, options);
+  const horrorData = await fetchTheHorror.json();
+  console.log(horrorData);
+  const categoryData = horrorData.map((data) => data.category);
+  const imgData = horrorData.map((data) => data.imageUrl);
+  selectedGenre.innerText = categoryData[1];
+  for (let i = 0; i < imgData.length; i++) {
+    movieCoverGenre[i].innerHTML = `<img class="movie-cover movie-cover-genre h-100" src="${imgData[i]}">`;
+  }
+  hiddenPrompts.forEach((prompt) => {
+    prompt.classList.remove("d-none");
+  });
+};
 
 const getComedyMovies = async () => {
-    const fetchTheLaughter = await fetch("https://striveschool-api.herokuapp.com/api/movies/comedy", options);
-    const laughData = await fetchTheLaughter.json();
-    const categoryData = laughData.map(data => data.category);
-    const imgData = laughData.map(data => data.imageUrl);
-    console.log(imgData);
-    selectedGenre.innerText = categoryData[0];
-    movieCoverGenre.forEach(img => {
-        img.innerHTML = `<img class="movie-cover movie-cover-genre" src="${imgData}">`
-    })
-    hiddenPrompts.forEach(prompt => {
-        prompt.classList.remove("d-none");
-    })
-}
+  const fetchTheLaughter = await fetch(
+    comedyUrl,
+    options
+  );
+  const laughData = await fetchTheLaughter.json();
+  const categoryData = laughData.map((data) => data.category);
+  const imgData = laughData.map((data) => data.imageUrl);
+  selectedGenre.innerText = categoryData[0];
+  for (let i = 0; i < imgData.length; i++) {
+    movieCoverGenre[i].innerHTML = `<img class="movie-cover movie-cover-genre h-100" src="${imgData[i]}">`;
+  }
+  hiddenPrompts.forEach((prompt) => {
+    prompt.classList.remove("d-none");
+  });
+};
 
 const getMusicMovies = async () => {
-    const fetchTheMusic = await fetch("https://striveschool-api.herokuapp.com/api/movies/music", options);
-    const musicData = await fetchTheMusic.json();
-    const categoryData = musicData.map(data => data.category);
-    const nameData = musicData.map(data => data.name);
-    const descriptionData = musicData.map(data => data.description);
-    const imgData = musicData.map(data => data.imageUrl);
-    console.log(imgData);
-    selectedGenre.innerText = categoryData[0];
-    movieCoverGenre.forEach(img => {
-        img.innerHTML = `<img class="movie-cover movie-cover-genre" src="${imgData}">`
-    })
-    hiddenPrompts.forEach(prompt => {
-        prompt.classList.remove("d-none");
-    })
+  const fetchTheMusic = await fetch(
+    musicUrl,
+    options
+  );
+  const musicData = await fetchTheMusic.json();
+  const categoryData = musicData.map((data) => data.category);
+  const nameData = musicData.map((data) => data.name);
+  const descriptionData = musicData.map((data) => data.description);
+  const imgData = musicData.map((data) => data.imageUrl);
+  selectedGenre.innerText = categoryData[0];
+  for (let i = 0; i < imgData.length; i++) {
+    movieCoverGenre[i].innerHTML = `<img class="movie-cover movie-cover-genre h-100 onclick=getDetails()" src="${imgData[i]}">`;
+  }
+  hiddenPrompts.forEach((prompt) => {
+    prompt.classList.remove("d-none");
+  });
+  console.log(nameData, descriptionData);
+};
+
+const details = async  () => {
+    if (selectedGenre.innerText === "horror") {
+        const fetchTheHorror = await fetch(horrorUrl, options);
+          const horrorData = await fetchTheHorror.json();
+          const categoryData = horrorData.map((data) => data.category);
+          const nameData = horrorData.map((data) => data.name);
+          const descriptionData = horrorData.map((data) => data.description);
+          const imgData = horrorData.map((data) => data.imageUrl);
+          const urlData = horrorData.map((data) => data._id);
+          for (let i=0; i<horrorData.length; i++) {
+          modalWindow.innerHTML = `<div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header d-flex">
+              <h5 class="modal-title">${nameData[i]}</h5>
+              <h5 class="modal-subtitle col-12">${categoryData[i]}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <img class="movie-cover movie-cover-genre h-100 onclick=getDetails()" src="${imgData[i]}">
+              <p>${descriptionData[i]}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <a class= "col btn btn-success" href="backoffice.html?appointmentId=${urlData[i]}">Edit</a>
+            </div>
+          </div>
+        </div>`
+        console.log(modalWindow);
+        modalWindow.classList.add("popup_active");
+          }
+    } else if (selectedGenre.innerText === "comedy") {
+        const fetchTheComedy = await fetch(comedyUrl, options);
+        const comedyData = await fetchTheComedy.json();
+        const categoryData = comedyData.map((data) => data.category);
+        const nameData = comedyData.map((data) => data.name);
+        const descriptionData = comedyData.map((data) => data.description);
+        const imgData = comedyData.map((data) => data.imageUrl);
+        const urlData = comedyData.map((data) => data._id);
+        for (let i=0; i<comedyData.length; i++) {
+          modalWindow.innerHTML = `<div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header d-flex">
+              <h5 class="modal-title">${nameData[i]}</h5>
+              <h5 class="modal-subtitle col-12">${categoryData[i]}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <img class="movie-cover movie-cover-genre h-100 onclick=getDetails()" src="${imgData[i]}">
+              <p>${descriptionData[i]}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <a class= "col btn btn-success" href="backoffice.html?appointmentId=${urlData[i]}">Edit</a>
+            </div>
+          </div>
+        </div>`
+        console.log(modalWindow);
+        modalWindow.classList.add("popup_active");
+    }} else {
+        const fetchTheMusic = await fetch(musicUrl, options);
+        const musicData = await fetchTheMusic.json();
+        const categoryData = musicData.map((data) => data.category);
+        const nameData = musicData.map((data) => data.name);
+        const descriptionData = musicData.map((data) => data.description);
+        const imgData = musicData.map((data) => data.imageUrl);
+        const urlData = musicData.map((data) => data._id);
+        for (let i=0; i<musicData.length; i++) {
+          modalWindow.innerHTML = `<div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header d-flex">
+              <h5 class="modal-title">${nameData[i]}</h5>
+              <h5 class="modal-subtitle col-12">${categoryData[i]}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <img class="movie-cover movie-cover-genre h-100 onclick=getDetails()" src="${imgData[i]}">
+              <p>${descriptionData[i]}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <a class= "col btn btn-success" href="backoffice.html?appointmentId=${urlData[i]}">Edit</a>
+            </div>
+          </div>
+        </div>`
+        console.log(modalWindow);
+        modalWindow.classList.add("popup_active");
+    }
+}
+}
+musicBtn.addEventListener("click", getMusicMovies);
+horrorBtn.addEventListener("click", getHorrorMovies);
+comedyBtn.addEventListener("click", getComedyMovies);
+
+for (let i=0; i<movieCoverGenre.length; i++) {
+    movieCoverGenre[i].addEventListener('click', details);
 }
 
+const closePopup = () => {
+    modalWindow.innerHTML = ``;
+    console.log(modalWindow);
+  };
 
-musicBtn.addEventListener('click', getMusicMovies);
-horrorBtn.addEventListener('click', getHorrorMovies);
-comedyBtn.addEventListener('click', getComedyMovies);
+
+  closeModal.addEventListener('click', closePopup);
