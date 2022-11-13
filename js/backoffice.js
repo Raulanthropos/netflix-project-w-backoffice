55e31e5c8e47012f6b104a880e51129f9c47641d
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-13 23:57:47 +0200

    edit button works

--- js/backoffice.js
+++ js/backoffice.js
@@ -4,6 +4,7 @@ const comedyMoviesUrl = "https://striveschool-api.herokuapp.com/api/movies/comed
 const musicMoviesUrl = "https://striveschool-api.herokuapp.com/api/movies/music";
 const params = new URLSearchParams(window.location.search)
 const movieId = params.get('movieId');
+const editBtn = document.getElementById("edit-button");
 
 
 const getallHorrorMovies = async() => {
@@ -72,16 +73,39 @@ const onFormSubmit = async (event) => {
   window.onload = async () => {
       // We are editing - let's get the event to edit,
       // and prefill the form with its info.
-      const response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/id/${movieId}`, {headers: {
+      const options = {
+        headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
         "Content-Type": "application/json"
-      }})
-      const movie = await response.json();
-      console.log(response, movie);
+      }}
 
- document.getElementById("e-name").value = movie.name;
- document.getElementById("description").value = movie.description;
- document.getElementById("category").value = movie.category;
- document.getElementById("image-url").value = movie.imageUrl;
-      };
+      const optionPut = {
+        method: 'PUT',
+        headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1N2Q0YmUzZDAwMTU4NDYwMGQiLCJpYXQiOjE2NjgwODUzMzUsImV4cCI6MTY2OTI5NDkzNX0.Gr11ODEGnFQLDHzWp7kIa-hafK6XaSswM75muzNXr5o",
+        "Content-Type": "application/json"
+      }}
+
+ if (movieId) {
+      const response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/id/${movieId}`, options);
+      const movie = await response.json();
+      document.getElementById("e-name").value = movie.name;
+      document.getElementById("description").value = movie.description;
+      document.getElementById("category").value = movie.category;
+      document.getElementById("image-url").value = movie.imageUrl;
+    }
+
+
+  const editMovie = async () => {
+    const response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/${movieId}`, optionPut);    
+    if (response.ok) {
+      alert("The movie was edited successfully!")
+    } else {
+      throw new Error("Error! Please try again!");
+    }
+  }
+
+  editBtn.addEventListener('click', editMovie);
+} 
